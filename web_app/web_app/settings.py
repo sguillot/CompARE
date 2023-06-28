50ab5d29be256a55a6c533b6aa9a133e05f869e6
sguillot <sebastien.guillot@irap.omp.eu>
2023-06-28 22:57:39 +0200

    Update settings.py
    MySQL password as environment variable

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME':'compare3',
         'HOST':'127.0.0.1',
         'USER':'root',
-        'PASSWORD':'$iutinfo',
+        'PASSWORD':os.environ.get('PWD_MYSQL'),
         'PORT':'3306'
     }
 }
