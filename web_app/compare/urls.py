49fed21cecc8258b07293e0ec147904e45dded7e
sguillot <sebastien.guillot@irap.omp.eu>
2023-08-03 12:24:35 +0200

    Various improvement of display, and added text
    Addresses:
    - issue #33 (text in "more info" and "home page")
    - issue #32
    - issue #31
    
    Various padding and display adjustments
    
    Various formatting python fixes

--- web_app/compare/urls.py
+++ web_app/compare/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("visu/", views.visu_data, name="visu"),
     path("visu/detail/<id>", views.detail, name="detail"),
-    path("login/",views.login,name="login"),
-    path("logout/",views.logout,name="logout"),
-    path("add/",views.insert_data,name="insert"),
-    path("info/",views.info,name="info"),
-    path("visu/detail/modify/<id>",views.modify,name="modify")
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("add/", views.insert_data, name="insert"),
+    path("info/", views.info, name="info"),
+    path("visu/detail/modify/<id>", views.modify, name="modify")
     ]
 
